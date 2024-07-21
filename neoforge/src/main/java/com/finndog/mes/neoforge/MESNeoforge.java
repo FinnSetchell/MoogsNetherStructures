45b01e594a6c423823a907e03a33bee35e06775d
FinnSetchell <88200552+FinnSetchell@users.noreply.github.com>
2024-07-21 17:53:00 +0100

    added neoforge compatibility for 1.20.4

---
+++ neoforge/src/main/java/com/finndog/mes/neoforge/MESNeoforge.java
@@ -0,0 +1,52 @@
+package com.finndog.mes.neoforge;
+
+import com.finndog.mes.MESCommon;
+import com.finndog.mes.events.lifecycle.RegisterReloadListenerEvent;
+import com.finndog.mes.events.lifecycle.ServerGoingToStartEvent;
+import com.finndog.mes.events.lifecycle.ServerGoingToStopEvent;
+import com.finndog.mes.events.lifecycle.SetupEvent;
+import com.finndog.mes.modinit.registry.neoforge.ResourcefulRegistriesImpl;
+import net.neoforged.bus.api.EventPriority;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.AddReloadListenerEvent;
+import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
+
+
+@Mod(MESCommon.MODID)
+public class MESNeoforge {
+
+    public MESNeoforge(IEventBus modEventBus) {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(EventPriority.NORMAL, ResourcefulRegistriesImpl::onRegisterForgeRegistries);
+
+        MESCommon.init();
+
+        IEventBus eventBus = NeoForge.EVENT_BUS;
+
+        modEventBus.addListener(MESNeoforge::onSetup);
+        eventBus.addListener(MESNeoforge::onServerStarting);
+        eventBus.addListener(MESNeoforge::onServerStopping);
+        eventBus.addListener(MESNeoforge::onAddReloadListeners);
+    }
+
+    private static void onSetup(FMLCommonSetupEvent event) {
+        SetupEvent.EVENT.invoke(new SetupEvent(event::enqueueWork));
+    }
+
+    private static void onServerStarting(ServerAboutToStartEvent event) {
+        ServerGoingToStartEvent.EVENT.invoke(new ServerGoingToStartEvent(event.getServer()));
+    }
+
+    private static void onServerStopping(ServerStoppingEvent event) {
+        ServerGoingToStopEvent.EVENT.invoke(ServerGoingToStopEvent.INSTANCE);
+    }
+
+    private static void onAddReloadListeners(AddReloadListenerEvent event) {
+        RegisterReloadListenerEvent.EVENT.invoke(new RegisterReloadListenerEvent((id, listener) -> event.addListener(listener)));
+    }
+
+}
